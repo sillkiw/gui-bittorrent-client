57343f38c312221d141115219b9d1d5083faa0a4
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-11-20 20:11:21 +0300

    Update showinfo.py

--- showinfo.py
+++ showinfo.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
+
 class ShowInfo(tk.Toplevel):
     def __init__(self,file_path,width_s,heigh_s):
          super().__init__()
@@ -11,9 +12,14 @@ class ShowInfo(tk.Toplevel):
 
          self.source_text = tk.Label(self,text="Sourse:")
          self.source_text.pack()
-
+      
+  
+        # Resizing image to fit on button 
          file_name = file_path.split("/")[-1]
-         self.source_button = ttk.Button(self,text=file_name,command= lambda  : self.change_torrent(file_path))
+         self.imag = tk.PhotoImage(file=r"images/torrent_icon.png")
+         self.imag = self.imag.subsample(-2,-2)
+         self.button_width  = self.winfo_screenwidth()//2
+         self.source_button = ttk.Button(self,text=file_name,width=self.button_width,image = self.imag,command= lambda  : self.change_torrent(file_path))
          self.source_button.pack()
          
        
