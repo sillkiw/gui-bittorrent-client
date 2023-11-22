a24096cefb02e18e906e2dd4c41d008d79074ed9
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-22 20:40:02 +0300

    Update showinfo.py

--- showinfo.py
+++ showinfo.py
@@ -1,22 +1,35 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
-class ShowInfo(tk.Toplevel):
-    def __init__(self,file_path,width_s,heigh_s):
+class TorrFile_InfoWindow(tk.Toplevel):
+    #Окно обзорщика торрент файла
+    def __init__(info,file_path,head_width,head_height):
          super().__init__()
          
-         self.title("File System inside torrent")
-         self.geometry(f"{width_s // 3}x{heigh_s // 3}")
-         self.file_path = file_path
+         info.title("Torrent File System ")
 
-         self.source_text = tk.Label(self,text="Sourse:")
-         self.source_text.pack()
+         info.info_width = head_width // 4 #Ширина окна
+         info.info_height = head_height // 4 #Высота окна
 
-         file_name = file_path.split("/")[-1]
-         self.source_button = ttk.Button(self,text=file_name,command= lambda  : self.change_torrent(file_path))
-         self.source_button.pack()
-         
+         #Центрирование по центру главного окна(head)
+         info.geometry(f"{info.info_width}x{info.info_height}+{(head_width-info.info_width)//2}+{(head_height-info.info_height)//2}")
        
+         #Путь до файла выбранного пользователя
+         info.file_path = file_path
+
+         #Графа для измения торрент-файла
+         tk.Label(info,text="Sourse:").pack()
+        
+         info.file_name = file_path.split("/")[-1] #Имя файла
+
+         #Кнопка для изменения торрент-файла в окне обзорщика торрента
+         info.style_button = ttk.Style()
+         info.configure('TButton', anchor=tk.W)
+         
+         
+    def set_button(info):
+        
+
 
     def change_torrent(self,source_path):
         target_file = fd.askopenfile(parent = self,initialdir=source_path,filetypes =[('Torrent Files', '*.torrent')]) 
