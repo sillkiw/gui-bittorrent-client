e932ef55ab7f1a7939b4be73b278245ee323c09e
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-23 01:11:00 +0300

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,1 +1,15 @@
+import bencode as ben   #Библиотека для бенкодирования(используется в метафайлах торрента)
 
+
+class Torrent:
+    def __init__(meta,file_path):
+        meta.torrent_path = file_path
+    def open_and_read_Metafile(meta) :
+        with open(meta.torrent_path,"rb") as torrent_file:
+            meta.metainfo = ben.bdecode(torrent_file.read())
+            meta.info = meta.metainfo['info']
+
+
+            print(type(meta.info))
+            print(meta.info)
+           
