5739818cfdf84a334fe6fe94f7b9e7c78ce1b300
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-24 01:42:48 +0300

    Create head_interface.py

---
+++ head_interface.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import ttk
+from showinfo import InfoWindow
+class HeadWindow(tk.Tk): #главное окно
+    def __init__(head):
+        super().__init__()
+        head.title('PirTorrent')
+        
+        head.head_width = head.winfo_screenwidth()   #ширина главного окна
+        head.head_height = head.winfo_screenheight() #высота главного окна
+        head.geometry(f"{head.head_width}x{head.head_height}")
+        
+        head.iconbitmap("images/icon.ico")
+        
+        
+        head.set_tool_bar() #Установка панели инструментов
+       
+
+
+    #Функция создания панели инструментов(Open|Edit|View)
+    def set_tool_bar(head):
+       
+        #Функция открытия файловой системы и выбора файла
+        def open_file_system(): 
+            #Пользователь выбирает торрент файл
+            target_file = fd.askopenfile(filetypes =[('Torrent Files', '*.torrent')]) 
+            if target_file is not None:
+                head.show_info_ab_file(target_file.name)
+                
+
+
+        #Инициализация панели инструментов (Open|Edit|View)
+        main_menu = tk.Menu(head) 
+
+        #Всплывающее окно для File
+        file_menu = tk.Menu(tearoff=0)
+        file_menu.add_command(label="Open...",command=open_file_system) #Open... открывает обзор файловой системы
+        file_menu.add_command(label="Open url...")
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit")
+
+        #Инициализация(File|Edit|View) 
+        main_menu.add_cascade(label="File",menu = file_menu)
+        main_menu.add_cascade(label="Edit")
+        main_menu.add_cascade(label="View")
+
+        #Бинд на главное окно
+        head.config(menu=main_menu)
+   
+    #Функция для вызова окна обзорщика файла
+    #@param file_name - путь до файла выбранного пользователем
+    def show_info_ab_file(head,file_path):
+        #Jткрытия окна обзорщика файловой системы торрент файла
+        #(!)(!)Реализовать проверку на дурака(пользователь добавляет один и тот же торрент несколько раз)(проверять части)
+        head.torrent_show = InfoWindow(head,file_path,head.head_width,head.head_height) 
+        head.torrent_show.grab_set()
+        head.torrent_show.mainloop()
+        head.check_user_action()
+    def check_user_action(head):
+        if head.torrent_show.state_of_answer == InfoWindow.T_OPENED:
+            window.frame = tk.LabelFrame(window,text="dsfs")
+            window.frame.pack()
+   
+        
+  
+
+
+        
+
+        
+
+if __name__ == "__main__":
+    window = HeadWindow()
+    window.mainloop()
