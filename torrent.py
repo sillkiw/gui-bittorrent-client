ba8f461589055c13468686c85117e4750213a740
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-24 15:04:08 +0300

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,6 +1,7 @@
 import bencode as ben   #Библиотека для бенкодирования(используется в метафайлах торрента)
-
-
+import hashlib as hash
+import random
+import requests as rq
 class Torrent:
     SINGLE_FILE = 1
     MULTIPLE_FILE = 0
@@ -21,10 +22,23 @@ class Torrent:
                 tr.kind_file = Torrent.MULTIPLE_FILE
             else:
                 tr.length = tr.info['length']
-                tr.piecies = tr.info['pieces']
+                tr.piecies = tr.info['pieces'] #!!!
                 tr.kind_file = Torrent.SINGLE_FILE
+            tr.prepare_request_GET()
+    def prepare_request_GET(tr):
+        tr.info_hash = hash.sha1(ben.bencode(tr.info)).digest()
+        tr.params = {
+            'info_hash':  tr.info_hash,
+            'peer_id' : '-PR7070-'+"".join([str(random.randint(0,9)) for _ in range(12)]),
+            'port' : 6889,
+            'uploaded' : 0,
+            'downloaded' : 0,
+            'left' : 0,
+            'event' : 'started'
 
-        
+        }
+    def make_request_GET(tr):
+        tr.tracker_response = rq.get(tr.announce,params = tr.params)
 
            
            
