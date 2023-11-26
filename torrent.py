cd4774229d0f9746043fcf69143cee92b222a106
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-26 17:39:53 +0300

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -3,6 +3,9 @@ import hashlib as hash
 import random
 import requests as rq
 import ipaddress as ip
+import socket
+import struct
+
 class Torrent:
     SINGLE_FILE = 1
     MULTIPLE_FILE = 0
@@ -21,17 +24,22 @@ class Torrent:
             if 'files' in tr.metainfo['info']:
                 tr.files = tr.info['files']
                 tr.kind_file = Torrent.MULTIPLE_FILE
+                tr.length = 0
+                for file in tr.files:
+                    tr.length += file['length']
             else:
                 tr.length = tr.info['length']
                 tr.piecies = tr.info['pieces'] #!!!
                 tr.kind_file = Torrent.SINGLE_FILE
             tr.prepare_request_GET()
+  
     def prepare_request_GET(tr):
         tr.info_hash = hash.sha1(ben.bencode(tr.info)).digest()
+        tr.peer_id = b'-PR7070-'+bytes([random.randint(0,9) for _ in range(12)])
         tr.params = {
             'info_hash':  tr.info_hash,
-            'peer_id' : '-PR7070-'+"".join([str(random.randint(0,9)) for _ in range(12)]),
-            'port' : 6889,
+            'peer_id' : tr.peer_id,
+            'port' : 6881,
             'uploaded' : 0,
             'downloaded' : 0,
             'left' : tr.length,
@@ -40,12 +48,28 @@ class Torrent:
         }
     def make_request_GET(tr):
         tr.tracker_response = rq.get(tr.announce,params = tr.params)
-        print(tr.tracker_response.content)
-        print(tr.tracker_response.request)
-        a = ben.bdecode(tr.tracker_response.content)
-        wor = a['peers']
-        print(wor[0])
-        print(len(wor))
+    def connect_with_peer(tr):
+      
+      
+        tr.resp_content = ben.bdecode(tr.tracker_response.content)
+        tr.list_peers = tr.resp_content['peers']
+        tr.make_clean()
+        print(tr.ls)
+        tr.handshake = b"\x13Bittorent Protocol\0\0\0\0\0\0\0\0"+tr.info_hash+tr.peer_id
+        tr.sock = socket.create_connection((tr.ls[2][0],tr.ls[2][1]))
         
-           
+      
+        
+
+    def make_clean(tr):
+          tr.ls = []
+          offset = 0
+          for _ in range(len(tr.list_peers)//6):
+                    ip = struct.unpack_from("!i", tr.list_peers, offset)[0]
+                    ip = socket.inet_ntoa(struct.pack("!i", ip))
+                    offset += 4
+                    port = struct.unpack_from("!H",tr.list_peers, offset)[0]
+                    offset += 2
+                    tr.ls.append([ip,port])
+                    
            
