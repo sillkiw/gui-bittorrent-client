f6d4b60c4e2086e8188579ea9415d2fd6e5154cd
AnatolYKovalenko <39805580+Anatol1YKovalenko@users.noreply.github.com>
2023-11-27 04:14:41 +0300

    Update head_interface.py

--- head_interface.py
+++ head_interface.py
@@ -2,33 +2,41 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import ttk
 from showinfo import InfoWindow
+from tracker import Tracker
+from peer_manager import PeerManager
 class HeadWindow(tk.Tk): #главное окно
+    HAS_DOWNLOAD = True
+    HAS_NOT_DOWNLOAD = False
     def __init__(head):
         super().__init__()
         head.title('PirTorrent')
-        
         head.head_width = head.winfo_screenwidth()   #ширина главного окна
         head.head_height = head.winfo_screenheight() #высота главного окна
         head.geometry(f"{head.head_width}x{head.head_height}")
-        
         head.iconbitmap("images/icon.ico")
+        head.status = head.HAS_NOT_DOWNLOAD
+
         #Установка просмотрщика файлов
         head.viewer = ttk.Treeview(head,show="headings")
         head.fill_viewer_collums()
         head.viewer.pack(fill=tk.BOTH,expand=1,pady=45)
-        head.set_tool_bar() #Установка панели инструментов
         
-       
-
+        #Установка панели инструментов
+        head.set_tool_bar() 
+        
+        head.check_status()
+    
+        
+    
     #Функция создания панели инструментов(Open|Edit|View)
     def set_tool_bar(head):
        
         #Функция открытия файловой системы и выбора файла
         def open_file_system(): 
             #Пользователь выбирает торрент файл
-            target_file = fd.askopenfile(filetypes =[('Torrent Files', '*.torrent')]) 
-            if target_file is not None:
-                head.show_info_ab_file(target_file.name)
+            head.target_file = fd.askopenfile(filetypes =[('Torrent Files', '*.torrent')]) 
+            head.show_info_ab_file()
+            head.check_status()
                 
 
 
@@ -77,30 +85,40 @@ class HeadWindow(tk.Tk): #главное окно
 
     #Функция для вызова окна обзорщика файла
     #@param file_name - путь до файла выбранного пользователем
-    def show_info_ab_file(head,file_path):
+    def show_info_ab_file(head):
         #Jткрытия окна обзорщика файловой системы торрент файла
         #(!)(!)Реализовать проверку на дурака(пользователь добавляет один и тот же торрент несколько раз)(проверять части)
-        head.torrent_show = InfoWindow(head,file_path,head.head_width,head.head_height) 
-        head.check_user_action()
+        if  head.target_file != None:
+            head.torrent_show = InfoWindow(head) 
+            head.check_user_action()
         
         
 
     def check_user_action(head):
         head.wait_window(head.torrent_show)
         if head.torrent_show.state_of_answer == InfoWindow.T_OPENED:
-            head.torrent_name = head.torrent_show.torrent_name
-            head.torrent_size = 0
-            head.torrent = head.torrent_show.opened_torrent
-            head.torrent.make_request_GET()
-            head.viewer.insert(parent="",index="end",values=(head.torrent_name,head.torrent_size,head.torrent.tracker_response.status_code,0,0,0,0,0))
+            head.torrent = head.torrent_show.torrent
+            head.viewer.insert(parent="",index="end",values=(head.torrent.name,0,0,0,0,0,0,0))
+            head.status = head.HAS_DOWNLOAD
             #info.file_system.insert(parent="",index = "end",iid=0,values = (n_ame,t_ype,s_ize))
-            head.torrent.connect_with_peer()
+           
         
-  
+    def check_status(head):
+        if head.status == head.HAS_DOWNLOAD:
+            head.initialize_tracker()
+            head.initialize_peer_manager()
 
 
-        
 
+    def initialize_tracker(head):
+        head.tracker = Tracker(head.torrent)
+        head.tracker.connect_with_tracker()
+        head.tracker.get_list_of_peers()
+        head.tracker.connect_with_peers()
+        head.tracker.make_messages()
+    def initialize_peer_manager(head):
+        head.peer_manager = PeerManager(head.tracker)
+        head.peer_manager.add_peers()       
         
 
 if __name__ == "__main__":
