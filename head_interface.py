75cf2e4ad677f6e0cf3e9a935053b3cfe5912b90
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 17:21:56 +0300

    Update head_interface.py

--- head_interface.py
+++ head_interface.py
@@ -2,63 +2,68 @@ import tkinter as tk
 from tkinter import filedialog as fd
 from tkinter import ttk
 from showinfo import InfoWindow
+from installation_manager import Installation_MNG
 import multiprocessing
-
 class HeadWindow(tk.Tk): #главное окно
-    HAS_DOWNLOAD = True
-    HAS_NOT_DOWNLOAD = False
     def __init__(head):
-        super().__init__()
+        super().__init__()    
         head.title('PirTorrent')
         head.head_width = head.winfo_screenwidth()   #ширина главного окна
         head.head_height = head.winfo_screenheight() #высота главного окна
         head.geometry(f"{head.head_width}x{head.head_height}")
         head.iconbitmap("images/icon.ico")
-        head.status = head.HAS_NOT_DOWNLOAD
-
-        #Установка просмотрщика файлов
+        #Установка просмотрщика установки
         head.viewer = ttk.Treeview(head,show="headings")
         head.fill_viewer_collums()
         head.viewer.pack(fill=tk.BOTH,expand=1,pady=45)
-        
         #Установка панели инструментов
         head.set_tool_bar() 
         
-        head.check_status()
-    
-        
-    
     #Функция создания панели инструментов(Open|Edit|View)
     def set_tool_bar(head):
-       
         #Функция открытия файловой системы и выбора файла
         def open_file_system(): 
             #Пользователь выбирает торрент файл
             head.target_file = fd.askopenfile(filetypes =[('Torrent Files', '*.torrent')]) 
             head.show_info_ab_file()
-            head.check_status()
-                
-
 
         #Инициализация панели инструментов (Open|Edit|View)
         main_menu = tk.Menu(head) 
-
         #Всплывающее окно для File
         file_menu = tk.Menu(tearoff=0)
         file_menu.add_command(label="Open...",command=open_file_system) #Open... открывает обзор файловой системы
         file_menu.add_command(label="Open url...")
         file_menu.add_separator()
         file_menu.add_command(label="Exit")
-
         #Инициализация(File|Edit|View) 
         main_menu.add_cascade(label="File",menu = file_menu)
         main_menu.add_cascade(label="Edit")
         main_menu.add_cascade(label="View")
-
         #Бинд на главное окно
         head.config(menu=main_menu)
-    
 
+    #Функция для вызова окна обзорщика файла
+    #@param file_name - путь до файла выбранного пользователем
+    def show_info_ab_file(head):
+        #Jткрытия окна обзорщика файловой системы торрент файла
+        #(!)(!)Реализовать проверку на дурака(пользователь добавляет один и тот же торрент несколько раз)(проверять части)
+        if  head.target_file != None:
+            head.torrent_show = InfoWindow(head) 
+            head.check_user_action()
+
+    #Проверка ответа пользователя     
+    def check_user_action(head):
+        #Ожидание ответа пользователя
+        head.wait_window(head.torrent_show)
+        if head.torrent_show.state_of_answer == InfoWindow._States_of_answer.T_OPENED:
+                head.torrent = head.torrent_show.torrent
+                #Инициализация установочного менеджера
+                head.installation_mng = Installation_MNG(head)
+                #Запуск нового потока
+                head.installation_mng.start()
+                head.viewer.insert(parent="",index = "end",values = (head.torrent.name,0,0,0,0,0,0,0))
+    
+    #Обзорщик установок
     def fill_viewer_collums(head):
         head.viewer['columns'] = ("Name","Size","Progress","Status","Seeds","Peers","Speed","Ratio")
         #Инициализация столбцов
@@ -80,56 +85,8 @@ class HeadWindow(tk.Tk): #главное окно
         head.viewer.heading("Seeds",text="Seeds",anchor="w")
         head.viewer.heading("Peers",text="Peers",anchor="w")
         head.viewer.heading("Speed",text="Speed",anchor="w")
-        head.viewer.heading("Ratio",text="Ratio",anchor="w")
-        
-
-    #Функция для вызова окна обзорщика файла
-    #@param file_name - путь до файла выбранного пользователем
-    def show_info_ab_file(head):
-        #Jткрытия окна обзорщика файловой системы торрент файла
-        #(!)(!)Реализовать проверку на дурака(пользователь добавляет один и тот же торрент несколько раз)(проверять части)
-        if  head.target_file != None:
-            head.torrent_show = InfoWindow(head) 
-            head.check_user_action()
-        
-        
-
-    def check_user_action(head):
-        head.wait_window(head.torrent_show)
-        if head.torrent_show.state_of_answer == InfoWindow._States.T_OPENED:
-            
-            
-            """
-            head.torrent = head.torrent_show.torrent
-            head.viewer.insert(parent="",index="end",values=(head.torrent.name,0,0,0,0,0,0,0))
-            head.status = head.HAS_DOWNLOAD
-            #info.file_system.insert(parent="",index = "end",iid=0,values = (n_ame,t_ype,s_ize))
-            """
-
-
-
-    """   
-    def check_status(head):
-        if head.status == head.HAS_DOWNLOAD:
-            head.initialize_tracker()
-            head.initialize_peer_manager()
-
-
-
-    def initialize_tracker(head):
-        head.tracker = Tracker(head.torrent)
-        head.tracker.connect_with_tracker()
-        head.tracker.get_list_of_peers()
-        head.tracker.connect_with_peers()
-        head.tracker.make_messages()
-
-
-    def initialize_peer_manager(head):
-        head.peer_manager = PeerManager(head.tracker)
-        head.peer_manager.add_peers()       
-    """    
+        head.viewer.heading("Ratio",text="Ratio",anchor="w") 
 
 if __name__ == "__main__":
-
     window = HeadWindow()
     window.mainloop()
