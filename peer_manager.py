6e788a37f116d7b82316ececd79413084af80751
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 17:21:52 +0300

    Update peer_manager.py

--- peer_manager.py
+++ peer_manager.py
@@ -1,4 +1,4 @@
-import multiprocessing
+
 class PeerManager:
     def __init__(pmg,tracker):
         pmg.tracker = tracker
@@ -8,12 +8,12 @@ class PeerManager:
             if pmg.handshake(peer):
                 pmg.peers.append(peer)
             else:
-                print(f"Can't handshake with {peer.id}")
+                print(f"Can't handshake with {peer.ip}")
     def handshake(pmg,peer):
         try:
             handshake_message = pmg.tracker.HANDSHAKE
             peer.sent_message(handshake_message)
-            print(f"new peer {peer.ip}")
+            print(f"HandShake with {peer.ip}")
             return True
         except Exception as e:
             print(f"Handshake error with {peer.ip}")
