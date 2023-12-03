a9f1d561419ba8d41e2c90367e4efdac0f4579ec
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 17:21:50 +0300

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,23 +1,16 @@
 import bencode as ben   #Библиотека для бенкодирования(используется в метафайлах торрента)
-import hashlib as hash
-import random
-import requests as rq
-import ipaddress as ip
-import socket
-import struct
 
 class Torrent:
-
-    class _States:
-        def __init__(state) :        
-            state.SINGLE_FILE = 1
-            state.MULTIPLE_FILE = 0
+    #Типы файла
+    class _Kinds_of_file:
+        MULTIPLE_FILE,SINGLE_FILE = (0,1)
+    
 
     def __init__(tr,file_path,name):
         tr.torrent_path = file_path
         tr.name = name
         
-    def read_Metafile(tr) :
+    def read_Metafile(tr):
         with open(tr.torrent_path,"rb") as torrent_file:
             tr.metainfo = ben.bdecode(torrent_file.read())
             tr.meta_keys = tr.metainfo.keys()
@@ -25,18 +18,18 @@ class Torrent:
             tr.announce = tr.metainfo['announce']
             tr.info = tr.metainfo['info']
             tr.piece_length = tr.info['piece length']
-            tr.piecies = tr.info['pieces']
+            tr.pieces = tr.info['pieces']
             tr.name = tr.info['name']
             if 'files' in tr.metainfo['info']:
                 tr.files = tr.info['files']
-                tr.kind_file = Torrent._States.MULTIPLE_FILE
+                tr.kind_file = Torrent._Kinds_of_file.MULTIPLE_FILE
                 tr.length = 0
                 for file in tr.files:
                     tr.length += file['length']
             else:
                 tr.length = tr.info['length']
-                tr.piecies = tr.info['pieces'] #!!!
-                tr.kind_file = Torrent._States.SINGLE_FILE
+                tr.pieces = tr.info['pieces'] #!!!
+                tr.kind_file = Torrent._Kinds_of_file.SINGLE_FILE
            
   
     
