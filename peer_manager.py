ee97d9da57ac300c883f9b3c6e1422bfa59dfda2
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 18:26:07 +0300

    Update peer_manager.py

--- peer_manager.py
+++ peer_manager.py
@@ -1,8 +1,9 @@
-
+from messages import impl_handshake_msg
 class PeerManager:
     def __init__(pmg,tracker):
         pmg.tracker = tracker
         pmg.peers = []
+        pmg.handshake_message = impl_handshake_msg(pmg.tracker.peer_id,pmg.tracker.info_hash)
     def add_peers(pmg):
         for peer in pmg.tracker.connected_peers:
             if pmg.handshake(peer):
@@ -11,8 +12,7 @@ class PeerManager:
                 print(f"Can't handshake with {peer.ip}")
     def handshake(pmg,peer):
         try:
-            handshake_message = pmg.tracker.HANDSHAKE
-            peer.sent_message(handshake_message)
+            peer.sent_message(pmg.handshake_message)
             print(f"HandShake with {peer.ip}")
             return True
         except Exception as e:
