e023c4569496d72aa45c065edd26183de647de12
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 17:21:54 +0300

    Update installation_manager.py

--- installation_manager.py
+++ installation_manager.py
@@ -1,7 +1,24 @@
 from tracker import Tracker
 from peer_manager import PeerManager
+from tracker import Tracker
+import threading
 
+class Installation_MNG(threading.Thread):
+    def __init__(imng,head):
+        threading.Thread.__init__(imng)
+        imng.head = head
+        #Инициализация трекера
+        imng.tracker = Tracker(head.torrent)
 
-class Installation_MNG(object):
-    def __init__(mng):
-        pass
+    #Переопределение метода run в Process
+    def run(imng):
+        imng.initialize_tracker_connection()
+        
+    
+    def initialize_tracker_connection(imng):
+        #Подключение к трекеру(отправка ему запроса), и получение списка пиров
+        imng.tracker.connect_with_tracker()
+        #Инициализация менеджера пиров
+        imng.peer_mng = PeerManager(imng.tracker)
+        imng.peer_mng.add_peers()
+        
