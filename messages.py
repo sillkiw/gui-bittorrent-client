9e93f9c9165ff84cb1187c40777515bbded83e5e
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-03 18:26:05 +0300

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,13 @@
+from struct import pack
+
+HS_PSTR = b"BitTorrent protocol"
+HS_PSTRLEN = len(HS_PSTR)
+def impl_handshake_msg(peer_id,info_hash):
+    reserved = b'\x00' * 8
+    return pack(">B{}s8s20s20s".format(HS_PSTRLEN),
+                         HS_PSTRLEN,
+                         HS_PSTR,
+                         reserved,
+                         info_hash,
+                         peer_id)
+    
