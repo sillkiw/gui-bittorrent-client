137fa7f9d0fff433000c6fc43628e6642a25a2a6
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-06 00:57:32 +0300

    Update installation_manager.py

--- installation_manager.py
+++ installation_manager.py
@@ -4,15 +4,16 @@ from tracker import Tracker
 import multiprocessing
 
 class Installation_MNG(multiprocessing.Process):
-    def __init__(imng,torrent,channel):
+    def __init__(imng,torrent,to_head):
         multiprocessing.Process.__init__(imng)
         #Инициализация трекера
         imng.tracker = Tracker(torrent)
-        imng.channel = channel
+        #Pipe c head
+        imng.to_head = to_head
     #Переопределение метода run в Process
     def run(imng):
         imng.initialize_tracker_connection()
-        imng.channel.send(imng.tracker.amount_of_connected_peers)
+        imng.to_head.send(imng.tracker.amount_of_connected_peers)
         
     def initialize_tracker_connection(imng):
         imng.tracker.connect_with_tracker()
