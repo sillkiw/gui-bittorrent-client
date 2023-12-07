097e2eba07976daa322de1f7eaeacedeb11fbde8
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-08 00:40:59 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,10 +1,10 @@
-from struct import pack
+from struct import pack,unpack
 
 
-#HandShake
+#HandShake <pstrlen><pstr><reserved><info_hash><peer_id>
 HS_PSTR = b"BitTorrent protocol"
 HS_PSTRLEN = len(HS_PSTR)
-def impl_handshake_msg(peer_id,info_hash):
+def handshake_msg_to_bytes(peer_id,info_hash):
     reserved = b'\x00' * 8
     return pack(">B{}s8s20s20s".format(HS_PSTRLEN),
                          HS_PSTRLEN,
@@ -13,4 +13,11 @@ def impl_handshake_msg(peer_id,info_hash):
                          info_hash,
                          peer_id)
 
-
+#KeepAlive keep-alive: <len=0000> нет message ID  нет payload
+KEEP_ALIVE_TOTAL_LENGTH = 4
+KEEP_ALIVE_PAYLOAD_LENGTH = 0
+def keep_alive_msg_from_bytes(payload):
+    payload_length = unpack(">I",payload[:KEEP_ALIVE_TOTAL_LENGTH])
+    if payload_length != KEEP_ALIVE_PAYLOAD_LENGTH:
+        return Exception("Принятое сообщение не \"KeepAlive\"" )
+    return KEEP_ALIVE_TOTAL_LENGTH
