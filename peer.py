8c18c148cdb273bf8c2c5437e02ad4cb3010116c
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-08 00:41:13 +0300

    Update peer.py

--- peer.py
+++ peer.py
@@ -7,6 +7,7 @@ class Peer:
         pr.read_buffer = b''
         pr.socket = None
         pr.live =False
+    
     def connect(pr):
         try:
             pr.socket = socket.create_connection((pr.ip,pr.port),timeout=0.3)
@@ -16,9 +17,12 @@ class Peer:
             print(f"NO CONNECTION {pr.ip}")
             return False
         return  True
+    
     def sent_message(pr,msg):
         try:
             pr.socket.send(msg)
         except Exception as e:
             pr.healthy = False
             logging.error("Failed to send to peer : %s" % e.__str__())
+    
+    def
