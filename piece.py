e13958b37b1c1b1e9235b08ade134e363f5d4c2c
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-11 18:29:53 +0300

    Update piece.py

---
+++ piece.py
@@ -0,0 +1,6 @@
+
+class Piece:
+    def __init__(pie,piece_index,piece_size,piece_hash):
+        pie.piece_index = piece_index
+        pie.piece_size = piece_size 
+        pie.piece_hash = piece_hash
