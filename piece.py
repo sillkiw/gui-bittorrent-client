a705583abc6dc790e4a17eb42a668784d16c4754
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-14 19:36:31 +0300

    Update piece.py

--- piece.py
+++ piece.py
@@ -1,3 +1,5 @@
+import math
+from block import BLOCK_SIZE,Block
 PIECE_SIZE = 20#(bytes) размер каждой части определен однозначно 
 
 
@@ -8,3 +10,17 @@ class Piece:
         pie.piece_hash = piece_hash
         pie.related_files = []
         pie.is_full = False
+        pie.number_of_blocks = int(math.ceil(float(piece_size)/BLOCK_SIZE))
+        pie.init_blocks()
+
+
+    def init_blocks(pie):
+        pie.blocks = []
+
+        if pie.number_of_blocks > 1:
+            for i in range(pie.number_of_blocks):
+                pie.blocks.append(Block())
+            if (pie.piece_size % BLOCK_SIZE) > 0:
+                pie.blocks[pie.number_of_blocks-1].block_size = pie.piece_size % BLOCK_SIZE
+        else:
+            pie.blocks.append(Block(block_size=int(pie.piece_size)))
