a5a2c3ef5204de1c81f94fb04e8b4da96ad9178e
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-14 14:24:59 +0300

    Update piece_manager.py

--- piece_manager.py
+++ piece_manager.py
@@ -7,6 +7,8 @@ class PieceManager:
         piemng.number_of_pieces = torrent.number_of_pieces
         piemng.bitfield = BitArray(piemng.number_of_pieces)
         piemng.initialize_pieces()
+        piemng.complete_pieces = 0
+        piemng._load_files()
     
     def initialize_pieces(piemng):
         piemng.pieces = []
@@ -22,4 +24,43 @@ class PieceManager:
                 last_pie_length = piemng.torrent.length - (piemng.number_of_pieces) * piemng.torrent.piece_length
                 piemng.pieces.append(piece.Piece(pie_index,last_pie_length,piemng.torrent.pieces[start:end]))
             else:
-                piemng.pieces.append(piece.Piece(pie_index,piemng.torrent.piece_length,piemng.torrent.pieces[start:end]))
+                piemng.pieces.append(piece.Piece(pie_index,piemng.torrent.piece_length,piemng.torrent.pieces[start:end])) 
+    
+    def _load_files(piemng):
+        piemng.files = []
+        piece_offset = 0
+        piece_size_used = 0
+
+        for f in piemng.torrent.file_names:
+            current_size_file = f["length"]
+            file_offset = 0
+
+            while current_size_file > 0:
+                id_piece = int(piece_offset / piemng.torrent.piece_length)
+                piece_size = piemng.pieces[id_piece].piece_size - piece_size_used
+
+                if current_size_file - piece_size < 0:
+                    file = {"length": current_size_file,
+                            "idPiece": id_piece,
+                            "fileOffset": file_offset,
+                            "pieceOffset": piece_size_used,
+                            "path": f["path"]
+                            }
+                    piece_offset += current_size_file
+                    file_offset += current_size_file
+                    piece_size_used += current_size_file
+                    current_size_file = 0
+
+                else:
+                    current_size_file -= piece_size
+                    file = {"length": piece_size,
+                            "idPiece": id_piece,
+                            "fileOffset": file_offset,
+                            "pieceOffset": piece_size_used,
+                            "path": f["path"]
+                            }
+                    piece_offset += piece_size
+                    file_offset += piece_size
+                    piece_size_used = 0
+
+                piemng.files.append(file)
