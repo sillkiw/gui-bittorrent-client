fa33c21e94a9f74ff4ab49b1ac2551ba0abee103
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-14 23:51:40 +0300

    Update piece.py

--- piece.py
+++ piece.py
@@ -1,6 +1,7 @@
-import math
-from block import BLOCK_SIZE,Block
-PIECE_SIZE = 20#(bytes) размер каждой части определен однозначно 
+import math,time
+from block import BLOCK_SIZE,Block,State
+
+PIECE_LEN = 20#(bytes) длина каждой части определен однозначно 
 
 
 class Piece:
@@ -24,3 +25,54 @@ class Piece:
                 pie.blocks[pie.number_of_blocks-1].block_size = pie.piece_size % BLOCK_SIZE
         else:
             pie.blocks.append(Block(block_size=int(pie.piece_size)))
+    
+    
+    def set_to_full(pie):
+        data = pie.merge_blocks()
+
+        if not pie.check_blocks(data):
+            pie.init_blocks()
+            return False
+        pie.is_full = True
+        pie.data = data
+        pie.put_piece_on_disk()
+        return True
+
+    
+    def put_piece_on_disk(pie):
+        for file in pie.related_files:
+            path_file = file['path']
+            file_offset = file['fileOffset']
+            piece_offset = file['pieceOffset']
+            legnth = file['length']
+
+            try:
+                f = open(path_file,'r+b')
+            except IOError:
+                f = open(path_file,'wb')
+            except Exception:
+                print("Не получается записать файл")
+                return
+            f.seek(file_offset)
+            f.write(pie.raw_data[piece_offset:piece_offset+legnth])
+            f.close()
+
+
+
+
+    def merge_blocks(pie):
+        buf = b''
+        for block in pie.blocks:
+            buf += block.data
+        return buf
+    
+    def get_block_from_free(pie):
+        if pie.is_full:
+            return None
+        
+        for i in range(0,len(pie.blocks)):
+            if pie.blocks[i].state == State.FREE:
+                pie.blocks[i].state = State.PEDNING
+                pie.blocks[i].last_seen = time.time()
+                block_offset = i * BLOCK_SIZE
+                return pie.piece_index, block_offset, pie.blocks[i].block_size
