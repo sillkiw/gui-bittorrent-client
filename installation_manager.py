84f93aa7f3128e1e9bcd91b825547eddacd92e5d
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-19 02:13:01 +0300

    Update installation_manager.py

--- installation_manager.py
+++ installation_manager.py
@@ -10,22 +10,16 @@ class Installation_MNG(multiprocessing.Process):
         multiprocessing.Process.__init__(imng)
         #Инициализация трекера
         imng.torrent = torrent
-        imng.tracker = Tracker(torrent)
         #Pipe c head
         imng.to_head = to_head
         imng.progress = 0
+        
 
     #Переопределение метода run в Process
     def run(imng):
 
-        imng.torrent.init_files()
-        imng.piece_mng = PieceManager(imng.torrent)
-
-        imng.initialize_tracker_and_peer_manager()
-        imng.peer_mng.get_on_well_piece_mng(imng.piece_mng)
-        
-    
-        imng.to_head.send(('0%','Downloading',f'0({imng.tracker.amount_of_connected_peers})'))
+        imng.initialize_tracker_and_managers()
+        imng.display_progress()
 
         while not imng.piece_mng.all_pieces_full():
             if not imng.peer_mng.has_unchoked_peers():
@@ -49,12 +43,13 @@ class Installation_MNG(multiprocessing.Process):
                 piece_index,block_offset,block_length = block_data  
                 request_msg = messages.request_msg_to_bytes(piece_index,block_offset,block_length)
                 peer.sent_message(request_msg)
-                imng.peer_mng.update_peers()
+            time.sleep(0.1)
             imng.display_progress()
+        
 
-            time.sleep(0.1)  
         imng.display_progress(status = 'Finished')
         imng.close()
+
     def display_progress(imng,status = 'Downloading'):
         update_progression = 0
         for i in range(imng.piece_mng.number_of_pieces):
@@ -69,10 +64,13 @@ class Installation_MNG(multiprocessing.Process):
         imng.to_head.send((str(percentage_completed)+"%",status,f'{number_of_active_peers}({number_of_inactive_peers})'))
 
 
-    def initialize_tracker_and_peer_manager(imng):
-        imng.tracker.connect_with_trackers()
+    def initialize_tracker_and_managers(imng):
+        imng.tracker = Tracker(imng.torrent)
+        imng.piece_mng = PieceManager(imng.torrent)
         imng.peer_mng = PeerManager(imng.tracker)
-        #Инициализация менеджера пиров
-        imng.peer_mng.handshake_with_peers()
+        imng.tracker.get_on_well_with_peer_mng(imng.peer_mng)
+        imng.tracker.connect_with_trackers()
+        imng.tracker.start()
+        
         
         
