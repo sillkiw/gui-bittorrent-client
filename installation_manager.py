7a2747558ae222c4d068ec7d95422b94d169a7b2
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-20 06:32:14 +0300

    Update installation_manager.py

--- installation_manager.py
+++ installation_manager.py
@@ -42,7 +42,7 @@ class Installation_MNG(multiprocessing.Process):
                 piece_index,block_offset,block_length = block_data  
                 request_msg = messages.request_msg_to_bytes(piece_index,block_offset,block_length)
                 peer.sent_message(request_msg)
-            time.sleep(0.01)
+            time.sleep(0.2)
             imng.peer_mng.update_peers()
             imng.display_progress()
         
