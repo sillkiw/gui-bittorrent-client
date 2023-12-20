d08d4a380dfaa28a30962b38c78d3a6b7b7becfa
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-20 06:32:10 +0300

    Update piece.py

--- piece.py
+++ piece.py
@@ -7,7 +7,7 @@ PIECE_LEN = 20#(bytes) длина каждой части определен о�
 class Piece:
     def __init__(pie,piece_index,piece_size,piece_hash):
         pie.piece_index = piece_index
-        pie.piece_size = piece_size 
+        pie.piece_size = piece_size
         pie.piece_hash = piece_hash
         pie.related_files = []
         pie.is_full = False
