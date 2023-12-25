d502431e8412b8f04945297582b24750f76da0a9
AnatolYKovalenko <Anatoly_Kovalenko_03@mail.ru>
2023-12-25 12:58:40 +0300

    Update install_form.py

--- install_form.py
+++ install_form.py
@@ -2,28 +2,30 @@ from installation_manager import Installation_MNG
 from multiprocessing import Pipe,Value
 import threading,os
 from enum import Enum
+from hurry.filesize import size,alternative
 #Установочная форма
 class InstallationForm:
    
     RUN,STOP,DELETE,FINISHED = range(0,4)
 
-    def __init__(install_form,head,torrent,id):
+    def __init__(install_form,head,torrent,id,size):
         install_form.head = head
         install_form.name,_ = os.path.splitext(torrent.name)
-        install_form.size = torrent.size
+        install_form.size = size
+        install_form.size_c = convert(install_form.size)
         install_form.id = id
         install_form.status = Value('i',InstallationForm.RUN)
         #Труба для обмена сообщениями между установочным процессорои и графическим интерфейсом
         install_form.to_head,install_form.from_install = Pipe()
         #Установочный процесс
-        install_form.installation_mng = Installation_MNG(torrent,install_form.to_head,install_form.status)
+        install_form.installation_mng = Installation_MNG(torrent,install_form.to_head,install_form.status,install_form.size)
         #Поток для изменения информации на экране
         install_form.updater_thread = threading.Thread(target=install_form.updater,daemon=True)
     
     #Размещение установочной формы на экран
     def pack_to_viewer(install_form):
         install_form.head.viewer.insert(parent="",index = "end",iid = install_form.id,
-                           values = (install_form.name,install_form.size,"0.0%","Initializing...","0(0)","∞"),tags = ('tor')) 
+                           values = (install_form.name,install_form.size_c,"0.0%","Initializing...","0(0)","∞"),tags = ('tor')) 
         
     #Обновление установочной формы
     def updater(install_form):
@@ -32,7 +34,7 @@ class InstallationForm:
                 #Получение информации от установки
                 progress,status,peers,speed = install_form.from_install.recv()
                 #Изменение информации об установки
-                install_form.head.viewer.item(install_form.id,values=(install_form.name,install_form.size,progress,status,peers,speed))
+                install_form.head.viewer.item(install_form.id,values=(install_form.name,install_form.size_c,progress,status,peers,speed))
             except Exception:
                 continue
 
@@ -41,4 +43,6 @@ class InstallationForm:
         install_form.updater_thread.start()
         #Запустить процесс начала установки
         install_form.installation_mng.start()
-        
+
+def convert(siz):
+    return size(siz,system=alternative)
